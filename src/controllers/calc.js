4a26d4aca91a234b2357f65d464a080611441384
Vinh Nghia <82487536+volevinhnghia@users.noreply.github.com>
2023-06-16 09:53:49 +0700

    Delete calc.js

--- src/controllers/calc.js
+++
@@ -1,7 +0,0 @@
-const calcTwoNumber = (req, res) => {
-  res.send((Number(req.query.numb1) + Number(req.query.numb2)).toString());
-  // Change two number in query from string to number, and then convert back to string and send result 
-};
-module.exports = {
-  calcTwoNumber,
-};

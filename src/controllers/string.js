0e94c5a99a4c91317fcf15879d48027d70908877
Vinh Nghia <vn29082000@gmail.com>
2023-06-16 10:55:16 +0700

    palindrome string
    done

--- src/controllers/string.js
+++ src/controllers/string.js
@@ -3,6 +3,27 @@ const reverseString = (req, res) => {
   //split each character with split method to array
   // then use reverse method for reverse array and then use join method for merge all element to one string
 };
+const palindromeString = (req, res) => {
+  let originalString = req.params.string;
+  let reverseString = req.params.string.split("").reverse().join(""); //reverse original string
+  let result = `${originalString} is palindrome string`; // result default value is palindrome string
+  for (let index = 0; index <= originalString.length / 2; index++) {
+    if (originalString[index] !== reverseString[index]) {
+      result = `${originalString} is not palindrome string`;
+      break;
+    }
+  }
+  res.send(result);
+};
+const elementCount = (req, res) => {
+    let newString = req.params.string;
+    let checkElement = req.query.ele;
+    console.log(newString);
+    console.log(checkElement);
+    res.send('work')
+}
 module.exports = {
   reverseString,
+  palindromeString,
+  elementCount
 };
--- src/routes/string.js
+++ src/routes/string.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const string = require('../controllers/string');
 
 router.get('/string/:string', string.reverseString);
+router.get('/palindromestring/:string', string.palindromeString);
+router.get('/string-element/:string', string.elementCount);
 
 module.exports = router;

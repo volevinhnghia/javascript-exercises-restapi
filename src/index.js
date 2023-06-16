18b00f04a51f5a4344b2d356d0f24b6f07a853f7
Vinh Nghia <vn29082000@gmail.com>
2023-06-16 10:03:35 +0700

    bigger number
    done bigger number

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const calcRouter = require("./routes/calc");
 const primeNumber = require("./routes/prime");
+const biggerNumber = require("./routes/biggerNumber");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
 app.use("/v1/", calcRouter);
 app.use("/v1/", primeNumber);
+app.use("/v1/", biggerNumber);
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });

1ced25f706d9195d53362605b96481e7b7f7b3ee
Vinh Nghia <82487536+volevinhnghia@users.noreply.github.com>
2023-06-16 10:04:58 +0700

    Delete calc.js

--- src/routes/calc.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-const calcTwoNumber = require('../controllers/calc');
-
-router.get('/calc', calcTwoNumber.calcTwoNumber);
-
-module.exports = router;
